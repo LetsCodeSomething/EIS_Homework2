2a697ce37ca70f24b5d76cf5aa51416050bb2096
LetsCodeSomething <mishaLySL@yandex.ru>
2024-04-14 17:07:30 +1000

    Add main table skeleton

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-//import reportWebVitals from './reportWebVitals';
+
+import './index.css';
+import dataset from './dataset/Dataset';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,4 +12,35 @@ root.render(
     </React.StrictMode>
 );
 
-root.render(<></>);
+const TableRow = (props) => {
+    const cells = Object.values(props.row).map((item, index) => <td key={index}> {item} </td>);
+
+    return (
+        <tr>
+            {cells}
+        </tr>
+    );
+}   
+
+const Table = (props) => {
+    const rows = dataset.slice(0, parseInt(props.rowsPerPage)).map((item, index) => <TableRow row={item} key={index}/>);
+
+    const pagesCount = Math.ceil(dataset.length * 1.0 / parseInt(props.rowsPerPage));
+
+    return (
+        <>
+            <h3>Данные по продажам в Walmart</h3>
+            <table className="table-data">
+                <thead>
+                    <TableRow row={Object.keys(dataset[0])} />
+                </thead>
+                <tbody>
+                    {rows}
+                </tbody>
+            </table>
+            {props.selectedPage}...{pagesCount}
+        </>
+    )
+}
+
+root.render(<><Table rowsPerPage="25" selectedPage="1"/></>);
